afe2d41fc5112b8d8f0cc090854d994a24ec3cc1
Malavika123434 <malavikarajesh2003@gmail.com>
2025-07-30 18:53:12 +0530

    Updated LINUX practice Day1,2,3 Word file

---
+++ stepin/sum.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int a,b,c;
+	a=90;
+	b=65;
+	c=a+b;
+	printf("sum= %d\n", c);
+	return 0;
+}
+
---
+++ stepin/test.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+ printf("welocome to linux\n");
+ int a=5,b=4,c;
+ c=a+b;
+ printf("sum=%d\n", c);
+ return 0;
+}
